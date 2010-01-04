f70fffb4d6e52aecf0e7e5af60a24faaff5168de
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-04 13:25:05 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_join_project do
+  name 'Redmine Join Project plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
