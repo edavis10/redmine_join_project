4c7ad4d7122af76c5c486dccf50a858a21adb7d5
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-04 13:42:09 -0800

    Added documentation structure.

--- init.rb
+++ init.rb
@@ -1,8 +1,13 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_join_project do
-  name 'Redmine Join Project plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Join Project'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-join'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'A Redmine plugin to allow non-members to join a project in Redmine'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+
 end
