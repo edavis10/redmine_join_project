eec18a08fe7b059505e30754634525d8ef6cbbf2
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-11 09:44:24 -0800

    [#3470] Make sure each user only has one request per project.

--- app/models/project_join_request.rb
+++ app/models/project_join_request.rb
@@ -4,6 +4,7 @@ class ProjectJoinRequest < ActiveRecord::Base
   belongs_to :project
 
   validates_inclusion_of :status, :in => ['new', 'accepted', 'declined'], :allow_blank => 'true'
+  validates_uniqueness_of :user_id, :scoped_to => :project_id
 
   def self.create_request(user, project)
     ProjectJoinRequest.create(:user => user, :project => project, :status => 'new')
