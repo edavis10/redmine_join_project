93ae5296c5b2f01a997957ddfd8a60fc77909343
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-11 17:43:23 -0800

    [#3472] Added a My Page block to list the current Join Queue.

--- app/models/project_join_request.rb
+++ app/models/project_join_request.rb
@@ -6,6 +6,23 @@ class ProjectJoinRequest < ActiveRecord::Base
   validates_inclusion_of :status, :in => ['new', 'accepted', 'declined'], :allow_blank => 'true'
   validates_uniqueness_of :user_id, :scoped_to => :project_id
 
+  named_scope :status_of, lambda { |status|
+    {
+      :conditions => {:status => status}
+    }
+  }
+
+  named_scope :visible_to, lambda {|user|
+    {
+      :include => :project,
+      :conditions => Project.allowed_to_condition(user, :approve_project_join_requests)
+    }
+  }
+  
+  def self.pending_requests_to_manage(user=User.current)
+    status_of('new').visible_to(user)
+  end
+  
   def self.create_request(user, project)
     ProjectJoinRequest.create(:user => user, :project => project, :status => 'new')
   end
--- test/unit/project_join_request_test.rb
+++ test/unit/project_join_request_test.rb
@@ -15,4 +15,35 @@ class ProjectJoinRequestTest < ActiveSupport::TestCase
       assert_sent_email
     end
   end
+
+  context "#pending_requests_to_manage" do
+    setup do
+      @project = Project.generate!(:project_subscription => 'request')
+      @manager = User.generate_with_protected!
+      @manager_role = Role.generate!(:permissions => [:approve_project_join_requests])
+      Member.generate!(:user_id => @manager.id, :project => @project, :roles => [@manager_role])
+
+      # New requests
+      @new1 = ProjectJoinRequest.generate!(:project => @project, :user => User.generate_with_protected!, :status => 'new')
+      @new2 = ProjectJoinRequest.generate!(:project => @project, :user => User.generate_with_protected!, :status => 'new')
+      # Old requests
+      @previous1 = ProjectJoinRequest.generate!(:project => @project, :user => User.generate_with_protected!, :status => 'accepted')
+      @previous2 = ProjectJoinRequest.generate!(:project => @project, :user => User.generate_with_protected!, :status => 'declined')
+
+      @other_project = Project.generate!(:project_subscription => 'request')
+      @other_join_request = ProjectJoinRequest.generate!(:project => @other_project, :user => User.generate_with_protected!, :status => 'new')
+
+      User.current = @manager
+    end
+    
+    should "only return new requests" do
+      assert_equal 2, ProjectJoinRequest.pending_requests_to_manage.count
+      assert ProjectJoinRequest.pending_requests_to_manage.include?(@new1)
+      assert ProjectJoinRequest.pending_requests_to_manage.include?(@new2)
+    end
+    
+    should "not include requests on projects the user doesn't have permission to access" do
+      assert !ProjectJoinRequest.pending_requests_to_manage.include?(@other_join_request)
+    end
+  end
 end
