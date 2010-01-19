5db6a32820be6ddad07df68856072c8375d13397
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-18 16:19:37 -0800

    [#3472] Added an integration test to check the request to join process

--- test/integration/request_to_join_test.rb
+++ test/integration/request_to_join_test.rb
@@ -5,16 +5,16 @@ class RequestToJoinTest < ActionController::IntegrationTest
   test "joining a project with request to join" do
     ActionMailer::Base.deliveries.clear
     setup_plugin_configuration
+    password = 'request'
     @project = Project.generate!(:project_subscription => 'request')
 
     # Manager to get emails
-    @manager = User.generate_with_protected!(:mail => 'manager@example.com')
+    @manager = User.generate_with_protected!(:login => 'manager', :mail => 'manager@example.com', :password => password, :password_confirmation => password)
     @manager.update_attributes(:mail_notification => true)
     @manager_role = Role.generate!(:permissions => [:approve_project_join_requests])
     Member.generate!(:user_id => @manager.id, :project => @project, :roles => [@manager_role])
     
     # Login
-    password = 'request'
     @user = User.generate_with_protected!(:password => password, :password_confirmation => password)
     log_user(@user.login, password)
     
@@ -35,5 +35,35 @@ class RequestToJoinTest < ActionController::IntegrationTest
 
     # Email sent
     assert_sent_email
+
+    # Logout to clear session
+    get 'logout'
+    assert_response :redirect
+    assert_equal User.anonymous, User.current
+
+    ## Manager to approve
+    log_user(@manager.login, password)
+    assert_equal @manager, User.current
+    
+    # Setup the My Page Block
+    xhr :post, "my/add_block", :block => "project-join-requests"
+
+    # Go to My Page
+    get 'my/page'
+    assert_response :success
+
+    click_link "Accept request"
+
+    # Redirected back to previous page
+    assert_equal current_url, url_for(:controller => 'my', :action => 'page')
+    assert_select 'div.flash.notice', /success/i
+
+    # Membership added
+    membership = Member.find_by_user_id_and_project_id(@user.id, @project.id)
+    assert_kind_of Member, membership
+    assert_equal 2, membership.roles.count
+    @configured_roles.each do |role|
+      assert membership.roles.include?(role)
+    end
   end
 end
