bd695e31f4f14d877c2f5a920d79af6761de80b9
Patrick Naubert <patrickn@xelerance.com>
2011-09-30 11:27:49 -0400

    Somehow addedd non-printable chars during my cut and paste.  fixed.

--- lib/join_project/patches/project_patch.rb
+++ lib/join_project/patches/project_patch.rb
@@ -3,9 +3,9 @@ module JoinProject
     module ProjectPatch
       def self.included(base)
         base.send(:include, InstanceMethods)
-        base.class_eval do
-          safe_attributes 'project_subscription'
-        end
+        base.class_eval do
+          safe_attributes 'project_subscription'
+        end
         base.extend(ClassMethods)
         base.class_eval do
           validates_inclusion_of :project_subscription, :in => join_options, :allow_nil => true, :allow_blank => 'true'
